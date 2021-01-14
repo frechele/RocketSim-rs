f5102ea8037e20b82018507e3a0d8ac39880d5fe
JYPark09 <jyp10987@gmail.com>
2021-01-14 14:15:48 +0000

    feat(physics): create Force structure

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -1,2 +1,3 @@
 pub mod atmosphere;
 pub mod gravity;
+pub mod types;
---
+++ src/physics/types.rs
@@ -0,0 +1,54 @@
+use std::ops::{Add, Sub};
+
+pub struct Force {
+	pub x: f32,
+	pub y: f32
+}
+
+impl Force {
+	pub fn create(length: f32, radian: f32) -> Self {
+		Self {
+			x: length * radian.cos(),
+			y: length * radian.sin()
+		}
+	}
+
+	pub fn length(&self) -> f32 {
+		(self.x * self.x + self.y * self.y).sqrt()
+	}
+
+	pub fn multiply(&self, v: f32) -> Self {
+		Self {
+			x: self.x * v,
+			y: self.y * v
+		}
+	}
+
+	pub fn normalize(self) -> Self {
+		let length = self.length();
+
+		self.multiply(1. / length)
+	}
+}
+
+impl Add for Force {
+	type Output = Self;
+
+	fn add(self, other: Self) -> Self {
+		Self {
+			x: self.x + other.x,
+			y: self.y + other.y
+		}
+	}
+}
+
+impl Sub for Force {
+	type Output = Self;
+
+	fn sub(self, other: Self) -> Self {
+		Self {
+			x: self.x - other.x,
+			y: self.y - other.y
+		}
+	}
+}
